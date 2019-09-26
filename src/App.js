781271dc42da14531aa9b9a0a86145d048520ece
ventulus95 <ventulus95@gmail.com>
2019-09-27 01:11:45 +0900

    "2019.09.26 두둔"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header.js';
+import Header from './Header';
 import DataTable from "./DataTable";
 // import ScatterChartBox from "./ScatterChartBox";
 import RadarChartBox from "./RadarChartBox";
